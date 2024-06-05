1b44659ce18bf20e3650a2b768f4d99713df030c
Korotsuuki <diacre.benoit@outlook.fr>
2024-06-05 05:41:50 +0200

    modification de fichiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import GradientCircle from './components/GradientCircle/GradientCircle.jsx'
-import GradientCircleBottom from './components/GradientCircleBottom/GradientCircleBottom.jsx'
 import LegalNotice from './pages/LegalNotice/LegalNotice.jsx'
 import Home from './pages/Home/Home.jsx'
 import Header from "./components/Header/Header.jsx"
@@ -15,7 +14,6 @@ function App() {
   return (
     <BrowserRouter basename={basename}>
       <GradientCircle />
-      <GradientCircleBottom/>
       <Header />
       <Routes>
         <Route path='/' element={<Home/>}/>
